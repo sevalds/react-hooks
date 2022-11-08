2477314806a86232075a4c19c71ba95876474cd9
se030 <se030.kim@gmail.com>
2022-11-08 09:41:16 +0900

    feat: useState practice
    https://www.youtube.com/watch?v=KJP1E-Y-xyo

--- src/hooks/useState.mjs
+++ src/hooks/useState.mjs
@@ -1,3 +1,74 @@
+const React = (function () {
+  function render(Component) {
+    idx = 0;
+    const C = Component();
+    C.render();
+    return C;
+  }
+
+  let hooks = [];
+  let idx = 0;
+  function useState(initVal) {
+    const state = hooks[idx] ?? initVal;
+
+    const _idx = idx;
+    const setState = (newVal) => (hooks[_idx] = newVal);
+    idx++;
+
+    return [state, setState];
+  }
+
+  function useEffect(cb, depArray) {
+    /*
+      to detect depArray changes, need to diff old dep and new dep
+      === need to keep our deps somewhere, hooks[]
+    */
+    const oldDeps = hooks[idx];
+
+    let hasChanged = true;
+    if (oldDeps) {
+      hasChanged = depArray.some((dep, i) => !Object.is(dep, oldDeps[i]));
+    }
+    if (hasChanged) cb();
+
+    hooks[idx] = depArray;
+    idx++;
+  }
+
+  return { render, useState, useEffect };
+})();
+
+function Component() {
+  const [count, setCount] = React.useState(1);
+  const [text, setText] = React.useState('init');
+
+  React.useEffect(() => console.log('I am useEffect'), [text]);
+
+  return {
+    render: () => console.log({ count, text }),
+    click: () => setCount(count + 1),
+    type: (letter) => setText(text + letter),
+  };
+}
+
+let App = React.render(Component);
+console.log();
+
+App.click();
+App = React.render(Component);
+console.log();
+
+App.type('s');
+App.click();
+App = React.render(Component);
+console.log();
+
+App.click();
+App = React.render(Component);
+console.log();
+
+/*
+
 export default function useState(initialValue) {
   let $rerenderTarget;
   console.log(this); // TODO: 훅을 사용하는 컴포넌트에 바인딩해서 리렌더 타겟에 접근 가능하도록
@@ -26,3 +97,5 @@ export default function useState(initialValue) {
 
   return [stateObject, setState];
 }
+
+*/
